218b705f3be25e2963719e953630a5981fa81ae4
yarikama <amaki30226@gmail.com>
2024-05-28 11:24:46 +0800

    new database divided

--- chroma.py
+++ chroma.py
@@ -1,32 +1,9 @@
+import os
+import shutil
 from langchain_community.vectorstores import Chroma
 from langchain.schema.document import Document
 from langchain.tools import tool
 from embedding import get_embedding
-import os
-import shutil
-
-@tool("查詢與給定文本相似的文件")
-def query_chroma(query_text: str, number_of_most_similar_results: int = 5) -> str:
-    """
-    Args:
-        query_text (str): 查詢文本。
-        number_of_most_similar_results (int, optional): 要返回的最相似文件的數量。默認為 5。
-
-    Returns:
-        str: 包含最相似文件內容的字符串，每個文件之間用 "\n\n---\n\n" 分隔。
-    """
-    vector_database = Chroma(
-        persist_directory="./chroma_db",
-        embedding_function=get_embedding(),
-    )
-    results = vector_database.similarity_search_with_score(
-        query_text,
-        k=number_of_most_similar_results
-    )
-    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
-    sources = [doc.metadata.get("id", None) for doc, _score in results]
-    context_text = f"來源: {sources}\n\n{context_text}"
-    return context_text
 
 def record_metadata_chunks(chunks: list[Document]) -> list[Document]:
     previous_page_id = None
@@ -41,9 +18,13 @@ def record_metadata_chunks(chunks: list[Document]) -> list[Document]:
         chunk.metadata["id"] = chunk_id
     return chunks
 
-def chroma_add_chunks(chunks: list[Document]) -> None:
+
+# 教科書分析師使用 chroma_answer 資料庫
+# 作業批改員使用 chroma_question 資料庫
+# 錯題整理師使用 chroma_textbook 資料庫
+def chroma_add_chunks(chunks: list[Document], database_directory: str) -> None:
     vector_database = Chroma(
-        persist_directory="./chroma_db",
+        persist_directory=database_directory,
         embedding_function=get_embedding(),
     )
     chunks_with_metadata = record_metadata_chunks(chunks)
@@ -66,10 +47,33 @@ def clean_vector_database() -> None:
         shutil.rmtree("chroma_db")
     print("已清空資料庫")
     
+@tool("查詢與給定文本相似的文件")
+def query_chroma(query_text: str, database_directory: str, number_of_most_similar_results: int = 5) -> str:
+    """
+    Args:
+        query_text (str): 查詢文本。
+        database_directory (str): 資料庫目錄，若是作業批改員，指定"chroma_answer"、若為錯題整理師指定 "chroma_question"、若為教科書分析師指定 "chroma_textbook" 。
+        number_of_most_similar_results (int, optional): 要返回的最相似文件的數量。默認為 5。
+
+    Returns:
+        str: 包含最相似文件內容的字符串，每個文件之間用 "\n\n---\n\n" 分隔。
+    """
+    vector_database = Chroma(
+        persist_directory=database_directory,
+        embedding_function=get_embedding(),
+    )
+    results = vector_database.similarity_search_with_score(
+        query_text,
+        k=number_of_most_similar_results
+    )
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    sources = [doc.metadata.get("id", None) for doc, _score in results]
+    context_text = f"來源: {sources}\n\n{context_text}"
+    return context_text
     
 if __name__ == "__main__":
     from document_process import DocumentProcessor
-    data_path = "data/exam_data"
+    data_path = "data/textbook_data"
     processor = DocumentProcessor(data_path)
     processed_documents = processor.process_documents()
-    chroma_add_chunks(processed_documents)  
+    chroma_add_chunks(processed_documents, "chroma_textbook")  
