1d806d5f68bb13af80cee852409ae7baf78f7ccb
yarikama <amaki30226@gmail.com>
2024-05-28 11:24:46 +0800

    new database divided

--- agents.py
+++ agents.py
@@ -12,11 +12,14 @@ class HomeworkCorrectionAgents():
     def program_manager_agent(self):
         return Agent(
             role='導師',
-            goal='協調作業批改專案並確保學生滿意度、以及學習成效',
+            goal='協調底下的教科書分析師、作業批改員和錯題本創建者，確保學生能有最佳的學習體驗，並最終報告給學生',
             backstory=dedent("""
-            作為項目經理，你的職責是監督整個作業批改專案，從啟動到最終報告。你將與各個團隊協調，確保順暢的溝通，並確保專案符合學生的期望和要求。
+            作為導師，你的職責是監督整個教育集團，從開始到最終報告給學生的報告。你將與各個團隊成員協調，確保順暢的溝通，並確保專案符合學生的期望和要求。
+            將學生問題分配交給教科書分析師，請它們分析學生問題，並依照高中教科書，指出問題和答案相關或是所屬的章節。
+            將學生的問題批改交給作業批改員，請它們批改作業並彙整錯誤概念的解釋。
+            將學生的錯誤概念給錯題整理者，請它們根據學生的錯誤概念搜尋相關的大學入學考試題目。
+            最後，將整個專案的結果以 .md 檔的形式報告給學生。
             """),
-            tools=[query_chroma],
             verbose=True,
             llm=self.llm
         )
@@ -24,9 +27,10 @@ class HomeworkCorrectionAgents():
     def textbook_analyst_agent(self):
         return Agent(
             role='教科書分析師',
-            goal='分析學生問題，並依照高中教科書，指出問題和答案相關或是所屬的章節',
+            goal='分析學生問題，並依照教科書內容，指出相關章節',
             backstory=dedent("""
-            作為教科書分析師，你的職責是收集和分析高中教科書的內容。你的分析將有助於確定學生的錯誤答案所屬的章節。
+            作為教科書分析師，你的職責是收集和分析高中教科書的內容。你的分析將有助於確定學生的錯誤答案所屬的章節，
+            並概述該章節需要的必要知識點和學習重點，並回傳給導師整理訊息。
             """),
             tools=[query_chroma],
             verbose=True,
@@ -38,7 +42,7 @@ class HomeworkCorrectionAgents():
             role='作業批改員',
             goal='批改作業並彙整錯誤概念的解釋',
             backstory=dedent("""
-            作為作業批改員，你的職責是批改學生的作業，並彙整他們錯誤概念的解釋。你的結果將輸出為 .md 檔案。
+            作為作業批改員，你的職責是批改學生的問題與答案，分析學生問題，並彙整他們錯誤概念的解釋，並回傳給導師整理訊息。
             """),
             tools=[query_chroma],
             verbose=True,
@@ -47,10 +51,10 @@ class HomeworkCorrectionAgents():
 
     def error_book_creator_agent(self):
         return Agent(
-            role='錯題本創建者',
-            goal='根據學生的錯誤概念搜尋相關的大學入學考試題目，並創建錯題本',
+            role='錯題整理者',
+            goal='根據學生的錯誤概念搜尋相關的考試、或是習作題目',
             backstory=dedent("""
-            作為錯題本創建者，你的職責是根據學生的錯誤概念搜尋相關的大學入學考試題目，並將其彙整為錯題本。錯題本將輸出為 .md 檔案。
+            作為錯題整理者，你的職責是根據學生的錯誤概念搜尋相關的考試或是習作題目，並回傳給導師整理訊息。
             """),
             tools=[query_chroma],
             verbose=True,
--- tasks.py
+++ tasks.py
@@ -6,15 +6,14 @@ class HomeworkCorrectionTasks():
     def project_initiation_task(self, agent, student_answer):
         return Task(
             description=dedent(f"""
-            透過審查專案範圍和學生需求來啟動作業批改專案。制定一個包含分析要求和任務分配的專案計劃。
+            透過審查專案範圍和學生需求來啟動團隊專案。制定一個包含分析要求和任務分配的專案計劃。
             學生的問題和他的答案：{student_answer}
             """),
             expected_output=dedent("""
-            一份全面的專案計劃，概述專案、分析要求以及每個團隊成員的任務分配。
+            一份詳盡的專案計劃，概述專案、分析要求以及每個團隊成員的任務分配，並指定他們要給你對於學生有幫助的內容。
             """),
             async_execution=True,
             agent=agent,
-            tools=[query_chroma]
         )
 
     def textbook_analysis_task(self, agent, student_answer):
@@ -24,7 +23,7 @@ class HomeworkCorrectionTasks():
             學生的問題和他的答案：{student_answer}
             """),
             expected_output=dedent("""
-            一份詳細的報告，包含教科書分析結果，指出與學生錯誤答案相關的章節。
+            一份詳細的報告，包含教科書分析結果，指出與學生錯誤答案相關的章節，必要知識點和學習重點，並回傳給導師整理訊息。
             """),
             async_execution=True,
             agent=agent,
@@ -34,11 +33,11 @@ class HomeworkCorrectionTasks():
     def homework_grading_task(self, agent, student_answer):
         return Task(
             description=dedent(f"""
-            批改學生的作業，並彙整他們錯誤概念的解釋，並補充相關其他也可能錯誤的章節類似題目。
+            批改學生的作業，並彙整他們錯誤概念的解釋，或是提出他們為什麼錯誤的易錯想法。
             學生的問題和他的答案：{student_answer}
             """),
             expected_output=dedent("""
-            總結作業批改結果，重點關注學生的錯誤概念和解釋。
+            總結作業批改結果，重點關注學生的錯誤概念和解釋其落入怎樣的考題陷阱當中。
             """),
             async_execution=True,
             agent=agent,
@@ -48,11 +47,11 @@ class HomeworkCorrectionTasks():
     def error_book_creation_task(self, agent, student_answer):
         return Task(
             description=dedent(f"""
-            根據學生的錯誤概念，搜尋相關的大學入學考試題目，並將其彙整為錯題本。將錯題本輸出為 .md 檔案。
+            根據學生的錯誤概念，搜尋相關考試題目或習題，並將其彙整起來，回傳給導師整理。
             學生的問題和他的答案：{student_answer}
             """),
             expected_output=dedent("""
-            一個 .md 檔案，包含根據學生錯誤概念而編製的錯題本，其中包括相關的大學入學考試題目。
+            包含根據學生錯誤概念而彙整的類似題型，或是同章節題目，其中包括其他試題和習題。
             """),
             agent=agent,
             tools=[query_chroma]
@@ -61,11 +60,11 @@ class HomeworkCorrectionTasks():
     def final_report_task(self, agent, student_answer):
         return Task(
             description=dedent(f"""
-            審查並整合教科書分析、作業批改和錯題本創建的結果。準備一份最終報告，提供對學生作業的總體評估，識別錯誤概念，並包含錯題本。
+            審查並整合教科書分析師、作業批改師和錯題整理師創建的結果。以他們的內容準備一份最終報告，提供對學生作業的總體評估，教科書章節與概念，並包含相近錯誤題型。
             學生的問題和他的答案：{student_answer}
             """),
             expected_output=dedent("""
-            一份全面的最終報告，包括教科書分析、作業批改結果、錯題本以及對學生作業的整體建議。
+            一份全面的最終報告，在審閱教科書分析、作業批改結果、錯題本以及對學生作業的整體建議過後，以精確並保留重要資訊的情況下，並以 .md 檔輸出。
             """),
             agent=agent,
             tools=[query_chroma]
